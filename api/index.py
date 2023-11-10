0dffaf52aa1846fe56f424f81be4c2352544c04d
herodendron <60262023+herodendron@users.noreply.github.com>
2023-11-10 09:17:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,4 +8,21 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'About (nice, you did it!)'
+    return 'About'
+
+@app.route('/get-weather', methods=['POST'])
+def get_weather():
+    # Expecting a JSON payload with a key 'location'
+    data = request.get_json()
+    location = data.get('location')
+
+    if not location:
+        return jsonify({"error": "Location is required"}), 400
+
+    # Example response, you would replace this with actual weather data retrieval logic
+    weather_data = {"location": location, "temperature": "23°C", "condition": "Sunny"}
+
+    return jsonify(weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
